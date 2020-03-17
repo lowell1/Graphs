651668528adc66010b9a892ca92279f6d4f707df
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-03-17 13:51:30 -0700

    graph 1 mvp

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,60 +13,148 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        res = set()
 
-    def bft(self, starting_vertex):
+        for vert_nbr_id in self.vertices[vertex_id]:
+            res.add(vert_nbr_id)
+
+        for vert_id in self.vertices:
+            vert = self.vertices[vert_id]
+
+            for vert_nbr_id in vert:
+                if vert_nbr_id == vertex_id:
+                    res.add(vert_id)
+                    break
+
+        return res
+
+    def bft(self, start_vert_id):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        vert_ids = Queue()
+        vert_ids.enqueue(start_vert_id)
+        visited = [start_vert_id]
 
-    def dft(self, starting_vertex):
+        while vert_ids.size() > 0:
+            vert_id = vert_ids.dequeue()
+            print(vert_id)
+
+            for vert_nbr_id in self.vertices[vert_id]:
+                if vert_nbr_id not in visited:
+                    visited.append(vert_nbr_id)
+                    vert_ids.enqueue(vert_nbr_id)
+
+
+    def dft(self, start_vert_id):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        vert_ids = Stack()
+        vert_ids.push(start_vert_id)
+        visited = [start_vert_id]
 
-    def dft_recursive(self, starting_vertex):
+        while vert_ids.size() > 0:
+            vert_id = vert_ids.pop()
+            print(vert_id)
+
+            for vert_nbr_id in self.vertices[vert_id]:
+                if vert_nbr_id not in visited:
+                    visited.append(vert_nbr_id)
+                    vert_ids.push(vert_nbr_id)
+
+    def dft_recursive(self, vert_id, visited = []):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
 
-    def bfs(self, starting_vertex, destination_vertex):
+        if len(visited) == 0:
+            print(vert_id)
+            visited = [vert_id]
+        
+        for vert_nbr_id in self.vertices[vert_id]:
+            if vert_nbr_id not in visited:
+                print(vert_nbr_id)
+                visited.append(vert_nbr_id)
+                self.dft_recursive(vert_nbr_id, visited)
+
+
+    def bfs(self, start_vert_id, dest_vert_id):
         """
         Return a list containing the shortest path from
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        prev = {}
+        vert_ids = Queue()
+        vert_ids.enqueue(start_vert_id)
+        visited = [start_vert_id]
 
-    def dfs(self, starting_vertex, destination_vertex):
+        while vert_ids.size() > 0:
+            vert_id = vert_ids.dequeue()
+            
+            for vert_nbr_id in self.vertices[vert_id]:
+                if vert_nbr_id not in visited:
+                    prev[vert_nbr_id] = vert_id
+                    vert_ids.enqueue(vert_nbr_id)
+                    visited.append(vert_nbr_id)
+
+        path = []
+        vert_id = dest_vert_id
+
+        while vert_id in prev:
+            path.append(vert_id)
+            vert_id = prev[vert_id]
+
+        return [start_vert_id] + [x for x in path[::-1]]
+
+    def dfs(self, start_vert_id, dest_vert_id):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        prev = {}
+        vert_ids = Stack()
+        vert_ids.push(start_vert_id)
+        visited = [start_vert_id]
 
-    def dfs_recursive(self, starting_vertex):
+        while vert_ids.size() > 0:
+            vert_id = vert_ids.pop()
+            
+            for vert_nbr_id in self.vertices[vert_id]:
+                if vert_nbr_id not in visited:
+                    prev[vert_nbr_id] = vert_id
+                    vert_ids.push(vert_nbr_id)
+                    visited.append(vert_nbr_id)
+
+        path = []
+        vert_id = dest_vert_id
+
+        while vert_id in prev:
+            path.append(vert_id)
+            vert_id = prev[vert_id]
+
+        return [start_vert_id] + [x for x in path[::-1]]
+
+    def dfs_recursive(self, cur_vert_id, dest_vert_id, path = [], visited = set()):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -74,7 +162,61 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+
+        # add cur_vert_id to visited
+
+        #append cur_vert_id to path
+
+        #if at dest return path
+
+        visited.add(cur_vert_id)
+        path = path + [cur_vert_id]
+        print(path)
+
+        if cur_vert_id == dest_vert_id:
+            return path
+
+        for vert_nbr_id in self.vertices[cur_vert_id]:
+            if vert_nbr_id not in visited:
+                new_path = self.dfs_recursive(vert_nbr_id, dest_vert_id, path, visited)
+
+                if new_path:
+                    return new_path
+
+        return None
+
+    # def dfs_recursive(self, cur_vert_id, dest_vert_id, prev = {}, visited = []):
+        """
+        Return a list containing a path from
+        starting_vertex to destination_vertex in
+        depth-first order.
+
+        This should be done using recursion.
+        """
+
+
+        # for vert_nbr_id in self.vertices[cur_vert_id]:
+        #     print(vert_nbr_id)
+        #     if vert_nbr_id not in visited:
+        #         visited.append(vert_nbr_id)
+        #         prev[vert_nbr_id] = cur_vert_id
+        #         self.dfs_recursive(vert_nbr_id, dest_vert_id, prev, visited)
+
+        # if cur_vert_id == dest_vert_id:
+        #     print("prev = ",prev)
+        #     path = []
+        #     vert_id = dest_vert_id
+
+
+        #     while vert_id in prev:
+        #         path.append(vert_id)
+        #         vert_id = prev[vert_id]
+        #         print("path = ", path)
+
+        #     return [cur_vert_id] + [x for x in path[::-1]]
+        # else:
+        #     return (prev, visited)
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
