ba86483798f8ea985a2bfa60376c88cc5a385794
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-03-19 22:45:31 -0700

    mvp

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from queue import Queue
 
 import random
 from ast import literal_eval
@@ -21,15 +22,70 @@ room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
-world.print_rooms()
+# world.print_rooms()
 
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
+traversal_path = ['n', 'n']
+
+backwards_directions = {
+    "n": "s",
+    "s": "n",
+    "e": "w",
+    "w": "e"
+}
+
+def traverse():
+    visited = []
+    path = []
+    last_direction = []
+
+    while True:
+        new_room = False
+        path.append(player.current_room.id)
+
+        if len(visited) == len(world.rooms):
+            break
+
+        for direction in player.current_room.get_exits():
+            room = player.current_room.get_room_in_direction(direction)
+
+            if room.id not in visited:
+                last_direction.append(direction)
+                visited.append(room.id)
+                player.travel(direction)
+                new_room = True
+                break
+
+        if new_room == False:
+            if last_direction == []:
+                break
+
+            player.travel(backwards_directions[last_direction[-1]])
+            last_direction.pop()
+
+    
+    moves = []
+
+    cur_room = world.starting_room
+
+    for idx in range(1, len(path)):
+        room_id = path[idx]
+
+        for direction in cur_room.get_exits():
+            room_in_direction = cur_room.get_room_in_direction(direction)
+
+            if room_in_direction.id == room_id:
+                moves.append(direction)
+                cur_room = room_in_direction
+                break
+
+    return moves
 
 
+
+traversal_path = traverse()
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,12 +107,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
